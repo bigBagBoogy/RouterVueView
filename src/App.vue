006487be8a3bc42833138baf5d2d3fd38ecd5305
maarten <van_den_bergmaarten@hotmail.com>
2023-11-20 16:09:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView} from 'vue-router'
+import Nav from './components/Nav.vue'
+
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <Nav/>
+        <RouterView/>
+    </div>
+  </header>
+  </template>
